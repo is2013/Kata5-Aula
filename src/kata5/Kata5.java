2363f2da4719e875bd39a85453c64ee50bdf6691
clocal <clocal@10.22.147.12>
2013-11-11 15:34:08 +0100

    first commit

---
+++ src/kata5/Kata5.java
@@ -0,0 +1,18 @@
+package kata5;
+
+public class Kata5 {
+
+    public static void main(String[] args) {
+        MockMailLoader loader = new MockMailLoader();
+        HistogramBuilder<Mail> builder = new HistogramBuilder<>(loader.load());
+        Histogram<String> histogram = builder.build(new AttributeExtractor<Mail,String>() {
+
+            @Override
+            public String extract(Mail mail) {
+                return mail.getDomain();
+            }
+        });
+        HistogramViewer<String> viewer = new SwingHistogramViewer<>(histogram);
+        viewer.show();
+    }
+}
